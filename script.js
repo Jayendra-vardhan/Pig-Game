faf48c4c191800e050af781938710a67aca37195
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-01-16 14:53:30 +0530

    debugged auto function running

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const btnNew= document.querySelector('.btn--new');
 const btnHold= document.querySelector('.btn--hold');
 const score0El= document.querySelector('#score--0');
 const currentScore0= document.getElementById('current--0');
+const player0= document.querySelector('.player--0');
 const score1El= document.getElementById('score--1');
 const currentScore1= document.getElementById('current--1');
+const player1= document.querySelector('.player--1');
 let score=0;
 let activePlayer=0;
 /*****************************************/
@@ -17,21 +19,30 @@ let activePlayer=0;
 //Functions
 //active player
 function changeActivePlayer(){
-
-}
+    if(activePlayer===0){
+        player0.classList.remove('player--active');
+        player1.classList.add('player--active');
+        activePlayer=1;
+    }
+    else{
+        player1.classList.remove('player--active');
+        player0.classList.add('player--active');
+        activePlayer=0;
+    }
+};
 
 //new game function
 function newGame(){
     score0El.textContent='0';
     score1El.textContent='0';
     diceEl.classList.add('hidden');
-}
+};
 
 //Counting Scores
 function scoreCount(earnedScored) {
     score+=earnedScored;
 
-}
+};
 
 //dice roll function
 function rollDice(){
@@ -42,17 +53,17 @@ function rollDice(){
 //  2.Displaying dice
     diceEl.src=`Pictures/dice-${diceNum}.png`;
     diceEl.classList.remove('hidden');
-
+    
 //  3. checking for 1(for player change)
-    if(diceEl===1){
+    if(diceNum===1){
         score=0;
         changeActivePlayer();
     }
     else{
-        scoreCount(earnedScored)
+        scoreCount(diceNum);
     }
-
-}
+    console.log(`score:${score}`,activePlayer+1);
+};
 
 /***************************************/
 
@@ -62,10 +73,12 @@ function rollDice(){
 newGame();
 
 //when the dice is rolled
-btnRoll.addEventListener('click',rollDice());
+btnRoll.addEventListener('click',rollDice);
 
 //when the new game is hit
-btnNew.addEventListener('click',newGame());
+btnNew.addEventListener('click',newGame);
 
 //when hold button is clicked
-btnHold.addEventListener('click',scoreCount());
+btnHold.addEventListener('click',function(){
+    changeActivePlayer()
+});
