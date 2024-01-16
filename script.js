b36131a9ce779d46d1f253f3c805457e99aadbeb
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-01-16 19:51:01 +0530

    Complete Game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //selecting elements
-/****************************************/
+/********************************************************/
 const diceEl= document.querySelector('.dice');
 const btnRoll= document.querySelector('.btn--roll');
 const btnNew= document.querySelector('.btn--new');
@@ -12,36 +12,52 @@ const player0= document.querySelector('.player--0');
 const score1El= document.getElementById('score--1');
 const currentScore1= document.getElementById('current--1');
 const player1= document.querySelector('.player--1');
-let score=0;
+let score=0,current0=0,current1=0;
 let activePlayer=0;
-/*****************************************/
+/*****************************************************/
+
 
 //Functions
+/*****************************************************/
 //active player
 function changeActivePlayer(){
     if(activePlayer===0){
         player0.classList.remove('player--active');
         player1.classList.add('player--active');
+        score0El.textContent=0;
         activePlayer=1;
     }
     else{
         player1.classList.remove('player--active');
         player0.classList.add('player--active');
+        score1El.textContent=0;
         activePlayer=0;
     }
 };
 
 //new game function
 function newGame(){
-    score0El.textContent='0';
-    score1El.textContent='0';
+    score0El.textContent=0;
+    score1El.textContent=0;
+    currentScore0.textContent=0;
+    currentScore1.textContent=0;
     diceEl.classList.add('hidden');
+    player1.classList.remove('player--active');
+    player0.classList.add('player--active');activePlayer=0;
+    score=0;
+    current0=0;
+    current1=0;
 };
 
 //Counting Scores
 function scoreCount(earnedScored) {
     score+=earnedScored;
-
+    if(activePlayer===0){
+        score0El.textContent=score;
+    }
+    else{
+        score1El.textContent=score;
+    }
 };
 
 //dice roll function
@@ -65,10 +81,28 @@ function rollDice(){
     console.log(`score:${score}`,activePlayer+1);
 };
 
-/***************************************/
+//setting current score
+function setCurrentScore(){
+    if(activePlayer===0){
+        current0+=score;
+        currentScore0.textContent=current0;
+        score=0;
+        score0El.textContent=0;
+        //checkwinner();
+    }
+    else{
+        current1+=score;
+        currentScore1.textContent=current1;
+        score=0;
+        score1El.textContent=0;
+        //checkwinner();
+    }
+}
+/**********************************************************/
+
 
 //Event listeners
-/**************************************/
+/**********************************************************/
 //at starting
 newGame();
 
@@ -80,5 +114,7 @@ btnNew.addEventListener('click',newGame);
 
 //when hold button is clicked
 btnHold.addEventListener('click',function(){
-    changeActivePlayer()
+    setCurrentScore();
+    changeActivePlayer();
 });
+/***************************************************************/
