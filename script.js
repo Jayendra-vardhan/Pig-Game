bbaccc173fb6696e903c18ed56e0a1be3565b55d
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-01-16 23:41:08 +0530

    updating experience

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const btnRoll= document.querySelector('.btn--roll');
 const btnNew= document.querySelector('.btn--new');
 const btnHold= document.querySelector('.btn--hold');
 const score0El= document.querySelector('#score--0');
-const current0EL= document.getElementById('current--0');
+const current0El= document.getElementById('current--0');
 const player0= document.querySelector('.player--0');
 const score1El= document.getElementById('score--1');
-const current1EL= document.getElementById('current--1');
+const current1El= document.getElementById('current--1');
 const player1= document.querySelector('.player--1');
 let score=0;
 let activePlayer=0;
@@ -21,26 +21,27 @@ let activePlayer=0;
 /*****************************************************/
 //active player
 function changeActivePlayer(){
+    score=0;
     if(activePlayer===0){
         player0.classList.remove('player--active');
         player1.classList.add('player--active');
-        score0El.textContent=0;
+        current0El.textContent=0;
         activePlayer=1;
     }
     else{
         player1.classList.remove('player--active');
         player0.classList.add('player--active');
-        score1El.textContent=0;
+        current1El.textContent=0;
         activePlayer=0;
     }
 };
 
 //new game function
 function newGame(){
+    current0El.textContent=0;
+    current1El.textContent=0;
     score0El.textContent=0;
     score1El.textContent=0;
-    current0EL.textContent=0;
-    current1EL.textContent=0;
     diceEl.classList.add('hidden');
     player1.classList.remove('player--active');
     player0.classList.add('player--active');
@@ -52,10 +53,10 @@ function newGame(){
 function scoreCount(earnedScored) {
     score+=earnedScored;
     if(activePlayer===0){
-        score0El.textContent=score;
+        current0El.textContent=score;
     }
     else{
-        score1El.textContent=score;
+        current1El.textContent=score;
     }
 };
 
@@ -71,27 +72,22 @@ function rollDice(){
     
 //  3. checking for 1(for player change)
     if(diceNum===1){
-        score=0;
         changeActivePlayer();
     }
     else{
         scoreCount(diceNum);
     }
-    console.log(`score:${score}`,activePlayer+1);
+    //console.log(`score:${score}`,activePlayer+1);
 };
 
 //setting current score
 function setCurrentScore(){
     if(activePlayer===0){        
-        current0EL.textContent=Number(current0EL.textContent)+score;
-        score=0;
-        score0El.textContent=0;
+        score0El.textContent=Number(score0El.textContent)+score;
         changeActivePlayer();
     }
     else{
-        current1EL.textContent=Number(current1EL.textContent)+score;
-        score=0;
-        score1El.textContent=0;
+        score1El.textContent=Number(score1El.textContent)+score;
         changeActivePlayer();
     }
 }
