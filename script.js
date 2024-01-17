7b2d74ae21542e86b95f15da1b9aadd4c8831654
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-01-17 05:34:40 +0530

    Adding winning condition

--- script.js
+++ script.js
@@ -19,6 +19,25 @@ let activePlayer=0;
 
 //Functions
 /*****************************************************/
+function winner(){
+    if(activePlayer===0){
+        player0.classList.add('player--winner');
+    }
+    else{
+        player1.classList.add('player--winner');
+    }
+    btnRoll.removeEventListener('click',rollDice);
+    btnHold.removeEventListener('click',setScore);
+}
+//checkwinner
+function checkwinner(){
+    if(Number(score0El.textContent)>=100||Number(score1El.textContent)>=100){
+        winner();
+    }
+    else{
+        changeActivePlayer(); 
+    }
+}
 //active player
 function changeActivePlayer(){
     currrentScore=0;
@@ -42,15 +61,26 @@ function newGame(){
     current1El.textContent=0;
     score0El.textContent=0;
     score1El.textContent=0;
-    diceEl.classList.add('hidden');
-    player1.classList.remove('player--active');
     player0.classList.add('player--active');
+    player1.classList.remove('player--active');
     activePlayer=0;
     currrentScore=0;
-};
+    diceEl.classList.add('hidden');
+    player0.classList.remove('player--winner');
+    player1.classList.remove('player--winner');
+
+    //when new game button is hit
+    btnNew.addEventListener('click',newGame);
+
+    //when the dice is rolled
+    btnRoll.addEventListener('click',rollDice);
+
+    //when hold button is clicked
+    btnHold.addEventListener('click',setScore);
+}newGame();
 
 //Counting Scores
-function scoreCount(earnedScored) {
+function currentScoreCount(earnedScored) {
     currrentScore+=earnedScored;
     if(activePlayer===0){
         current0El.textContent=currrentScore;
@@ -75,36 +105,19 @@ function rollDice(){
         changeActivePlayer();
     }
     else{
-        scoreCount(diceNum);
+        currentScoreCount(diceNum);
     }
     //console.log(`score:${currrentScore}`,activePlayer+1);
 };
 
 //setting current score
-function setCurrentScore(){
+function setScore(){
     if(activePlayer===0){        
         score0El.textContent=Number(score0El.textContent)+currrentScore;
-        changeActivePlayer();
     }
     else{
         score1El.textContent=Number(score1El.textContent)+currrentScore;
-        changeActivePlayer();
     }
+    checkwinner();
 }
 /**********************************************************/
-
-
-//Event listeners
-/**********************************************************/
-//at starting
-newGame();
-
-//when the dice is rolled
-btnRoll.addEventListener('click',rollDice);
-
-//when the new game is hit
-btnNew.addEventListener('click',newGame);
-
-//when hold button is clicked
-btnHold.addEventListener('click',setCurrentScore);
-/***************************************************************/
